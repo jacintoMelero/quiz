7b2467c5c8632e82561aab9526ede265721a44a0
xacy <xacyst@gmail.com>
2015-07-09 16:02:42 +0200

    fin tema 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,14 +1,34 @@
+var models=require('../models/models.js');
 //get quizes question
-exports.question = function (req , res ){
-  console.log('controller');
-  res.render('quizes/question',{pregunta:'¿Capital de Italia?'});
+
+exports.load = function (req , res ,next,quizId){
+	models.Quiz.find(quizId).then(function(quiz){
+		if(quiz){
+			req.quiz=quiz;
+		}
+		else{
+			next(new Error('No existe quiz '+quizId));
+		}
+	}).catch(function(error){next(error)});
+};
+exports.index = function (req , res ){
+	models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/inex.ejs',{quizes:quizes);
+	});
+};
+exports.show = function (req , res ){
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+		res.render('quizes/show',{quiz:req.quiz);
+	});
 };
 //get quizes answer
 exports.answer = function (req , res ){
-  if(req.query === 'Roma'){
-    res.render('quizes/answer',{respuesta:'Correcta'});
-  }
-  else{
-    res.render('quizes/answer',{respuesta:'Incorrecta'});
-  }
+  models.Quiz.find(req.params.quizId).success(function(quiz){
+    if(req.query.respuesta === req.quiz.respuesta){
+      res.render('quizes/answer',{quiz:req.quiz,respuesta:'Correcta'});
+    }
+    else{
+      res.render('quizes/answer',{quiz:req.quiz,respuesta:'Incorrecta'});
+    }
+  });
 };
