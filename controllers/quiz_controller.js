e022cc8be712efe90879c0a136cd4467b9b99ce9
xacy <xacyst@gmail.com>
2015-07-10 17:01:01 +0200

    testing

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -18,7 +18,7 @@ exports.index = function (req , res ){
 };
 exports.show = function (req , res ){
 	console.log('fuera');
-	models.Quiz.find(req.params.quizId).then(function(quiz){
+	models.Quiz.findById(req.params.quizId).then(function(quiz){
 		console.log('dentro');
 		res.render('quizes/show',{quiz: quiz});
 	});
